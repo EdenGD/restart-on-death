2a39e988eb986935d4cba7f7f0bf92564dcd7ed3
Eden <90335268+EdenGD@users.noreply.github.com>
2024-11-20 15:47:08 +0100

    Add Force Quit Flag
    This makes the shutdown command now forcibly close all programs without showing the "Closing app and restarting" or whatever screen, which also gives a chance to click cancel to go back without fully restarting.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@ class $modify(PlayerObject) {
 	void playerDestroyed(bool a) {
 		PlayerObject::playerDestroyed(a);
 
-		system("shutdown -r -t 0"); // the fun part
+		system("shutdown -r -f -t 0"); // the fun part
 	}
 };
